ce096c493b6ae5b61851a38303b0736045a156d7
zhengpanone <zhengpanone@hotmail.com>
2022-11-10 23:45:26 +0800

    feate: pinia的入门使用

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,16 @@
 <template>
-    <div>首页</div>
+    <h1>首页</h1>
+    <div>
+        {{ userStore.name }}的年龄：{{ userStore.age }}<br />
+        <button @click="addAge">点击给{{ userStore.name }}的年龄增加一岁</button>
+    </div>
 </template>
 <script lang="ts" setup>
+import { useUserStore } from '@/store/user'
+const userStore = useUserStore()
+const addAge = () => {
+    userStore.updateAge(++userStore.age)
+}
 </script>
 <style scoped>
 
