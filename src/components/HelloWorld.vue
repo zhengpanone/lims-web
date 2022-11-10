058b6fa689f98af1b81e50f68e0bc2c64cbe7319
zhengpanone <zhengpanone@hotmail.com>
2022-11-10 23:09:14 +0800

    JSX、TSX使用

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,20 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-
+import Foo from '@/components/foo'
+import Coo from '@/components/coo'
 defineProps<{ msg: string }>()
 
 const count = ref(0)
 </script>
-
 <template>
   <h1>{{ msg }}</h1>
-
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <Foo />
+  <Coo msg="Hell TSX" />
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+
 </style>
