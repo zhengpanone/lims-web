a7167634a05f97eb3de66cbfc4b1f1e6c89a0d83
zhengpanone <zhengpanone@hotmail.com>
2022-11-11 12:57:54 +0800

    feate: 接口类型处理

--- src/api/common.ts
+++ src/api/common.ts
@@ -4,8 +4,18 @@
 import request from '@/utils/request'
 
 export const getLoginInfo = () => {
-  return request({
+  /* return request({
     method: 'GET',
     url: '/user/login/info',
-  })
+  }) */
+  return request.get<{
+    status: number
+    msg: string
+    data: {
+      slide: string[]
+      logo_square: string
+      logo_rectangle: string
+      login_login: string
+    }
+  }>('/user/login/info')
 }
