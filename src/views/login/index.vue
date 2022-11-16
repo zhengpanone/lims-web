4a994ed4b0c2c3dfa042c3b54eb681ea349bac5f
zhengpanone <zhengpanone@hotmail.com>
2022-11-17 00:17:41 +0800

    feate: 登录页面搭建

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,63 @@
 <template>
-    <div>登录</div>
+    <div class="login-container">
+        <el-form class="login-form" ref="form" :rules="rules" :model="user" size="medium">
+            <div class="login-form__header">
+                <img class="login-logo" src="@/assets/login_logo.png" alt="拉勾心选">
+            </div>
+            <el-form-item prop="account">
+                <el-input v-model="user.account" placeholder="请输入用户名">
+                    <template #prefix>
+                        <el-icon>
+                            <User />
+                        </el-icon>
+                    </template>
+                </el-input>
+            </el-form-item>
+            <el-form-item prop="pwd">
+                <el-input v-model="user.pwd" type="password" placeholder="请输入密码">
+                    <template #prefix>
+                        <el-icon>
+                            <Lock />
+                        </el-icon>
+                    </template>
+                </el-input>
+            </el-form-item>
+            <el-form-item prop="imgcode">
+                <div class="imgcode-wrap">
+                    <el-input v-model="user.imgcode" placeholder="请输入验证码">
+                        <template #prefix>
+                            <el-icon>
+                                <Lock />
+                            </el-icon>
+                        </template>
+                    </el-input>
+                    <img class="imgcode" alt="验证码">
+                </div>
+            </el-form-item>
+            <el-form-item>
+                <el-button class="submit-button" type="primary" native-type="submit" :loading="loading">
+                    登录
+                </el-button>
+            </el-form-item>
+        </el-form>
+    </div>
 </template>
 <script lang="ts" setup>
 import { getLoginInfo } from '@/api/common'
 import type { ILoginInfo } from '@/api/types/common'
-import { onMounted, ref } from 'vue'
+import { onMounted, reactive, ref } from 'vue'
+import { User, Lock, Key } from '@element-plus/icons-vue';
 
+const user = reactive({
+    account: 'admin', pwd: '123456', imgcode: 'qazw'
+})
+const loading = ref(false)
+
+const rules = reactive({
+    account: [{ required: true, message: '请输入账号', trigger: 'change' }],
+    pwd: [{ required: true, message: '请输入密码', trigger: 'change' }],
+    imgcode: [{ required: true, message: '请输入验证码', trigger: 'change' }],
+})
 const list = ref<ILoginInfo['slide']>([])
 onMounted(() => {
     getLoginInfo().then(res => {
@@ -14,6 +66,55 @@ onMounted(() => {
     })
 })
 </script>
-<style scoped>
+<style scoped lang="scss">
+.login-container {
+    min-width: 400px;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #2d3a4b;
+}
 
+.login-form {
+    padding: 30px;
+    border-radius: 6px;
+    background: #fff;
+    min-width: 350px;
+
+    .login-form__header {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding-bottom: 30px;
+    }
+
+    .el-form-item:last-child {
+        margin-bottom: 0;
+    }
+
+    .login__form-title {
+        display: flex;
+        justify-content: center;
+        color: #fff;
+    }
+
+    .submit-button {
+        width: 100%;
+    }
+
+    .login-logo {
+        width: 271px;
+        height: 74px;
+    }
+
+    .imgcode-wrap {
+        display: flex;
+        align-items: center;
+
+        .imgcode {
+            height: 37px;
+        }
+    }
+}
 </style>
